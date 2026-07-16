9420027f1aabeb4bb0a80792eeb5917591c1d0e2
Júlio Peniche <76125283+JulioPeniche@users.noreply.github.com>
2026-07-16 13:15:01 -0300

    Delete SQL directory

--- SQL/04_ticket_medio.sql
+++
@@ -1,8 +0,0 @@
--- Fase 5.4
--- Ticket Médio
-
-SELECT
-  COUNT(DISTINCT order_id) AS pedidos,
-  ROUND(SUM(receita), 2) AS receita_total,
-  ROUND(SUM(receita) / COUNT(DISTINCT order_id), 2) AS ticket_medio
-FROM `marketing_analytics.fact_sales`;
